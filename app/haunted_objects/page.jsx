7e929ed3131047387f10b29632f55fec6cda51cf
alwxths <leongyuyao008@gmail.com>
2026-01-30 23:44:11 +0800

    finalised the stylesheet for both haunted objects and types of supernatural beings

--- app/haunted_objects/page.jsx
+++ app/haunted_objects/page.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./styles.modules.css"
 
 export default function Objects() {
     const hauntedObjects = [
@@ -39,66 +40,28 @@ export default function Objects() {
 
    return (
     
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "linear-gradient( to bottom, #020617, #020617)",
-        paddingBottom: "4rem",
-        width:"100%",
-      }}
-    >
+    <div className="container">
       {/* Top bar */}
-      <div
-        style={{
-          height: "60px",
-          background: "linear-gradient(to right, #04214a, #888)",
-        }}
+      <div className="topBar"
       />
-      <h1 style={{
-        textAlign: "center",
-        margin: "5rem ",
-        color: "#ffffff",
-        marginBottom:"3rem",
-        fontSize: 30,
-      }}
+      <h1 className="title"
       
-      >Top 6 Haunted Objects</h1>
-      <ul   style={{
-         listStyle: "none",
-         padding: "0 2rem",
-         display: "grid",
-         gridTemplateColumns: "repeat(3, 1fr)",
-         gap: "2rem",
-         }}>
+      >Haunted Objects</h1>
+      <ul className="grid">
         {hauntedObjects.map((place, index) => (
           <li
               key={index}
-              style={{
-              backgroundColor: "#1b1832",
-              marginBottom: "2rem",
-              padding: "1rem",
-              borderRadius: "8px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",   // centers everything horizontally
-              textAlign: "center",
-              //boxShadow: "0 0 10px rgba(0,0,0,0.5)",
-            }}
+              className="card"
           >
           <Link href={`/haunted_objects/${place.id}`}>
-              <Image
+              <img
               src={place.image}
               alt={place.name}
               width={420}
               height={300}
-              style={{
-                      objectFit: "cover",
-                      alignItems:"center",
-                      borderRadius: "6px",
-                      cursor: "pointer",
-                    }}  />
+              className="cardImage"/>
             </Link>
-            <h2 style={{ color: "#ffffff",fontSize:"1rem", }}>{index + 1}. {place.name}</h2>
+            <h2 className="cardTitle">{index + 1}. {place.name}</h2>
           </li>
         ))}
       </ul>
