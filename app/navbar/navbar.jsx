b445bef5e47fbe20cf72e150e871b94cf2b95d82
alwxths <leongyuyao008@gmail.com>
2026-01-31 20:01:12 +0800

    added the format of the website name and logo above the navbar

--- app/navbar/navbar.jsx
+++ app/navbar/navbar.jsx
@@ -1,15 +1,27 @@
-//test
+"use client";
 
-"use client"; 
-import styles from "./navbar.module.css"
-import {useState} from "react";
+import styles from "./navbar.module.css";
+import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
-
-export default function Navbar(){
-    const [dropdownvisible, setdropdownvisible] = useState(false);
-    return(
-    <nav className={styles.navbar}>
+export default function Navbar() {
+  const [dropdownvisible, setdropdownvisible] = useState(false);
+  return (
+    <>
+      <div className={styles.header}>
+        <Link href="/" className={styles.headerLink}>
+          <Image
+            src="/photos/ghost-8250317_640.png"
+            alt="Logo"
+            width={48}
+            height={48}
+            className={styles.logo}
+          />
+          <span className={styles.siteName}>Ghost Evidence</span>
+        </Link>
+      </div>
+      <nav className={styles.navbar}>
     <Link className={styles.navitem} href="/"> Home </Link>
     <Link className={styles.navitem} href="/ghost_equipment"> Ghost Equipment </Link>
     <Link className={styles.navitem} href="/haunted_places"> Haunted Places </Link>
@@ -38,7 +50,7 @@ export default function Navbar(){
         
         
     </div>
-
-    </nav>
-    )
+      </nav>
+    </>
+  );
 }
